8e458dcc2d4202f9207c4a7a50a74de0af26c7a5
Lais <kleid75@gmail.com>
2024-02-12 23:17:48 -0300

    fixing public imports

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "./header.styles";
-import Logo from "../../../public/assets/icons/cisarte-logo.png";
+import Logo from "../../../../public/assets/icons/cisarte-logo.png";
 import { createGlobalStyle } from 'styled-components'
 
 
