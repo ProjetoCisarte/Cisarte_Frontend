58abc8b3174d8ed16cdb50207b05ae4240ad61ed
Lilian Carvalho <kleidk74@gmail.com>
2024-02-19 13:33:06 -0300

    componentizing the address

--- src/pages/contactUs/contact-us-style.ts
+++ src/pages/contactUs/contact-us-style.ts
@@ -64,33 +64,3 @@ export const ContainerLinksContact = styled.div `
     }
 `;
 
-export const ContainerAddress = styled.div `
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    max-width: 1400px;
-    margin: 4rem auto;
-    flex-wrap: wrap;
-
-    iframe {
-        border-radius: 40px;
-        width: 620px;
-        height: 370px;
-    }
-
-    @media screen and (max-width: 685px) {
-        iframe {
-        width: 500px;
-    }
-    }
-
-    @media screen and (max-width: 540px) {
-        iframe {
-        width: 90%;
-    }
-
-    p {
-        max-width: 300px;
-    }
-    }
-`;
--- src/pages/contactUs/contact-us.tsx
+++ src/pages/contactUs/contact-us.tsx
@@ -1,11 +1,12 @@
 import Button from "../../components/atoms/button/button"
+import Address from "../../components/molecules/address/address"
 import { Banner } from "../../components/molecules/banner/banner"
 import Footer from "../../components/molecules/footer/footer"
 import Header from "../../components/molecules/header/header"
 import InputsTextLarge from "../../components/molecules/inputsTextLarge/inputs-text-large"
 import InputsTextarea from "../../components/molecules/inputsTextarea/inputs-textarea"
 import FormStructure from "../../components/organisms/formStructure/form-structure"
-import { ContainerIconsSocial, ContainerLinksContact, ContainerLinks, ContainerAddress } from "./contact-us-style"
+import { ContainerIconsSocial, ContainerLinksContact, ContainerLinks } from "./contact-us-style"
 
 const ContactUs = () => {
     return(
@@ -53,25 +54,7 @@ const ContactUs = () => {
             </ContainerLinksContact>
         </ContainerLinks>
 
-        <ContainerAddress>
-            <section>
-                <div>
-                    <h3>Como chegar</h3>
-                    <p>Viaduto Pedroso, 111 - Bela Vista, São Paulo - SP, CEP: 01322-010</p>
-                </div>
-                <div>
-                    <h3>Horário de Funcionamento</h3>
-                    <p>Aberto de terça a sexta das 9:00 às 17:00</p>
-                </div>
-            </section>
-            <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.156286743735!2d-46.64282888909096!3d-23.56282977871038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59a4e8eddcb3%3A0x3969cac9a2222d51!2sViaduto%20Pedroso%2C%20111%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001322-010!5e0!3m2!1spt-BR!2sbr!4v1708145205524!5m2!1spt-BR!2sbr"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            title="Google Maps"
-            ></iframe>
-        </ContainerAddress>
+        <Address />
 
         <Footer />
 
