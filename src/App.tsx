39947e85f0295c830e3e3760e35234393678b33e
Matheus Passos <matheuspassos.work@gmail.com>
2024-02-19 23:50:03 -0300

    Feat: add a dialog card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/molecules/header/header'
 import InputsTextLarge from './components/molecules/inputsTextLarge/inputsTextLarge'
 import InputsTextSmall from './components/molecules/inputsDateSmall/inputsDateSmall'
+import { DialogCard } from './components/molecules/dialog/dialog-card'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Header />
     <InputsTextLarge NameLabel='Nome' Placeholder='Digite seu nome'/>
     <InputsTextSmall NameLabel='Data de nascimento'/>
+    <DialogCard content='A música, presente na vida de todos, desperta emoções, ritmo e estimula a prática física. O contato com instrumentos musicais no CISARTE desenvolve percepções sensoriais e emocionais, proporcionando diversão e experiências não vividas na rua. A oficina é aberta a pessoas em situação de rua interessadas em música e canto.' imgAlt='' imgSrc='/assets/imgs/music.png' title='Música' />
     </>
   )
 }
