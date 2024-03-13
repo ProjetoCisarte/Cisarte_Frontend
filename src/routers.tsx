7b1fc0c6aefc4afdaed84eb7869ee99cb954c605
Vitória Florêncio <110254387+Vtoriaa@users.noreply.github.com>
2024-03-13 09:21:23 -0300

    creating new routes

--- src/routers.tsx
+++ src/routers.tsx
@@ -5,14 +5,19 @@ import WellnessAndHealthWorkshop from "./pages/wellnessAndHealthWorkshop/wellnes
 import ServiceWorkshops from "./pages/serviceWorkshops/service-workshops";
 import LanguageWorkshops from "./pages/languageWorkshops/language-workshops";
 import ContactUs from "./pages/contactUs/contact-us";
+import { Home } from "./pages/home/home";
+import { AboutUs } from "./pages/aboutUs/about";
+import { Donation } from "./pages/donation/donation";
 
 function RouterApp() {
 
     return (
       <Router>
         <Routes>
-          {/* <Route path="/" element={<Home />}>Home</Route> */}
+          <Route path="/" element={<Home />}>Home</Route>
+          <Route path="/doacao" element={<Donation />}></Route>
           <Route path="/voluntariado" element={<Voluntary />}></Route>
+          <Route path="/sobre-nos" element={<AboutUs />}></Route>
           <Route path="/oficinaDeArtes" element={<ArtWorkshop />}></Route>
           <Route path="/oficinaBemEstareSaude" element={<WellnessAndHealthWorkshop />}></Route>
           <Route path="/oficinaServicos" element={<ServiceWorkshops />}></Route>
